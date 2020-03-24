e3c3679b4111a5ec832e2294dac79428325a89c9
Jason Irish <jirish@dynamicagency.com>
2020-03-23 20:34:50 -0500

    FEATURE ElementListLayoutExtension
    closes #7

---
+++ src/Extension/ElementListLayoutExtension.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Dynamic\SiteTools\Extension;
+
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+
+/**
+ * Class ElementListLayoutExtension
+ * @package Dynamic\SiteTools\Extension
+ */
+class ElementListLayoutExtension extends DataExtension
+{
+    /**
+     * @var array
+     */
+    private static $db = [
+        'Columns' => 'Enum("3,4,6,8,9,10,12")',
+    ];
+
+    /**
+     * @var array
+     */
+    private static $defaults = [
+        'Columns' => 12,
+    ];
+
+    /**
+     * @param FieldList $fields
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $cols = [
+            '3' => 'One Quarter',
+            '4' => 'One Third',
+            '6' => 'Half',
+            '8' => 'Two Thirds',
+            '9' => 'Three Quarters',
+            '10' => '10 Columns Centered',
+            '12' => 'Full'
+        ];
+
+        $fields->addFieldToTab(
+            'Root.Layout',
+            DropdownField::create(
+                'Columns',
+                'Columns',
+                $cols
+            )->setEmptyString('select')
+        );
+    }
+}
