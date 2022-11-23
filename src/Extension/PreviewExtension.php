ffb795f1315eec90cae5b9295b84190e41dfc60b
Jason Irish <jirish@dynamicagency.com>
2022-11-23 15:59:16 -0600

    phpcs

--- src/Extension/PreviewExtension.php
+++ src/Extension/PreviewExtension.php
@@ -135,8 +135,7 @@ class PreviewExtension extends DataExtension
     {
         if (!$this->owner->AbstractFirstParagraph && $this->owner->Abstract) {
             return $this->owner->Abstract;
-        } elseif (
-            (
+        } elseif ((
                 !$this->owner->AbstractFirstParagraph && !$this->owner->Abstract
             ) || $this->owner->AbstractFirstParagraph
         ) {
